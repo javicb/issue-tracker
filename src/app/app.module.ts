b8d3ff24e6f685da2e16a296999278655edd36dd
Javier Cruz <javi.cb@gmail.com>
2021-09-05 19:23:24 +0200

    creating report issue form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 import { IssueListComponent } from './issue-list/issue-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IssueListComponent
+    IssueListComponent,
+    IssueReportComponent
   ],
   imports: [
     BrowserModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
